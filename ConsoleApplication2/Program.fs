ad1e347e24e0b08dc98aaf7a77e250139984837c
Sean Safari <safaris1@tcnj.edu>
2013-03-16 18:37:24 -0400

    Changes

--- ConsoleApplication2/Program.fs
+++ ConsoleApplication2/Program.fs
@@ -15,53 +15,29 @@ open Casanova.Drawing
 open Casanova.StandardLibrary
 open Casanova.StandardLibrary.Core
 
-    type [<CasanovaWorld>] World = {
-      Ship : Ship
-      Alien : Alien
-      Alien1 : Alien
+    type [<CasanovaWorld>] World = { 
+      Bar : Bar
     }
 
-    and [<CasanovaEntity>] Ship = {
+    and [<CasanovaEntity>] Bar = {
          Position : Rule<Vector2<m>>
          Velocity : Rule<Vector2<m/s>>
          Sprite   : DrawableSprite
     } with
-      static member Position'(self:Ship,dt:float32<s>) = !self.Position + dt * !self.Velocity 
-      static member Velocity'(self:Ship,dt:float32<s>) = !self.Velocity * 0.9f
-      static member SpritePosition'(self:Ship) = !self.Position * 1.0f<pixel/m>
-
-    and [<CasanovaEntity>] Alien = {
-      Position : Rule<Vector2<m>>
-      Life     : Var<float32>
-      Sprite   : DrawableSprite
-    } with
-     static member Position'(self:Alien,dt:float32<s>) = !self.Position + dt * Vector2<m/s>.UnitY * 40.0f
-
-     static member SpritePosition'(self:Alien) = !self.Position * 1.0f<pixel/m>
+      static member Position'(self:Bar,dt:float32<s>) = !self.Position + dt * !self.Velocity 
+      static member Velocity'(self:Bar,dt:float32<s>) = !self.Velocity * 0.9f
+      static member SpritePosition'(self:Bar) = !self.Position * 1.0f<pixel/m>
 
 let start_game (game:StartGameArgs) =
 
   let world0 = 
     {
-      Ship = 
+      Bar = 
         {
-          Position = Rule.Create(Vector2<m>(0.0f<m>,450.0f<m>))
+          Position = Rule.Create(Vector2<m>(490.0f<m>,0.0f<m>))
           Velocity = Rule.Create(Vector2<m/s>.Zero)
-          Sprite   = DrawableSprite.Create(game.default_layer, Vector2<pixel>.One, Vector2<pixel>.One * 100.0f, @"newship")
+          Sprite   = DrawableSprite.Create(game.default_layer, Vector2<pixel>.One, Vector2<pixel>.One * 100.0f, @"p1")
         }
-       
-      Alien =
-       {
-          Position = Rule.Create(Vector2<m>(250.0f<m>,-400.0f<m>))
-          Life = Var.Create(1.0f)
-          Sprite   = DrawableSprite.Create(game.default_layer, Vector2<pixel>.One, Vector2<pixel>.One * 100.0f, @"alien1")
-       }
-      Alien1 =
-       {
-          Position = Rule.Create(Vector2<m>(250.0f<m>,-300.0f<m>))
-          Life = Var.Create(1.0f)
-          Sprite   = DrawableSprite.Create(game.default_layer, Vector2<pixel>.One, Vector2<pixel>.One * 100.0f, @"alien1")
-       }
     }
   let inline (!) x = immediate_lookup x
   let main = yield_
@@ -71,10 +47,10 @@ let start_game (game:StartGameArgs) =
       wait_key_press Keys.Escape  =>> game.quit()
       wait_key_press Keys.F9      =>> game.save "savefile"
       wait_key_press Keys.F10     =>> game.load "savefile"
-      wait_key_down Keys.D        => co{ world0.Ship.Velocity := !world0.Ship.Velocity + Vector2<m/s>.UnitX * 100.0f }
-      wait_key_down Keys.A        => co{ world0.Ship.Velocity := !world0.Ship.Velocity - Vector2<m/s>.UnitX * 100.0f }
-     // wait_key_down Keys.W        => co{ world0.Ship.Velocity := !world0.Ship.Velocity - Vector2<m/s>.UnitY * 100.0f }
-     // wait_key_down Keys.S        => co{ world0.Ship.Velocity := !world0.Ship.Velocity + Vector2<m/s>.UnitY * 100.0f }
+     // wait_key_down Keys.D        => co{ world0.Bar.Velocity := !world0.Bar.Velocity + Vector2<m/s>.UnitX * 100.0f }
+     // wait_key_down Keys.A        => co{ world0.Bar.Velocity := !world0.Bar.Velocity - Vector2<m/s>.UnitX * 100.0f }
+      wait_key_down Keys.W        => co{ world0.Bar.Velocity := !world0.Bar.Velocity - Vector2<m/s>.UnitY * 100.0f }
+      wait_key_down Keys.S        => co{ world0.Bar.Velocity := !world0.Bar.Velocity + Vector2<m/s>.UnitY * 100.0f }
     ]
   world0,main,input
 
