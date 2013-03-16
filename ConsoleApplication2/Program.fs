4954d353229976942d2c106ad5fa855bf7ddaa50
jackjamieson <jamieson.jack@gmail.com>
2013-03-16 20:19:15 -0300

    Update Program.fs

--- ConsoleApplication2/Program.fs
+++ ConsoleApplication2/Program.fs
@@ -1,4 +1,4 @@
-﻿// Learn more about F# at http://fsharp.net
+// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
@@ -15,42 +15,108 @@ open Casanova.Drawing
 open Casanova.StandardLibrary
 open Casanova.StandardLibrary.Core
 
-    type [<CasanovaWorld>] World = { 
-      Bar : Bar
+    type [<CasanovaWorld>] World = {
+      p1 : p1
+      p2 : p2
+      ball: ball
+      wall: wall
+      wall2: wall2
     }
 
-    and [<CasanovaEntity>] Bar = {
+    and [<CasanovaEntity>] p1 = {
          Position : Rule<Vector2<m>>
          Velocity : Rule<Vector2<m/s>>
          Sprite   : DrawableSprite
     } with
-      static member Position'(self:Bar,dt:float32<s>) = !self.Position + dt * !self.Velocity 
-      static member Velocity'(self:Bar,dt:float32<s>) = !self.Velocity * 0.9f
-      static member SpritePosition'(self:Bar) = !self.Position * 1.0f<pixel/m>
+      static member Position'(self:p1,dt:float32<s>) = !self.Position + dt * !self.Velocity
+      static member Velocity'(self:p1,dt:float32<s>) = !self.Velocity * 0.9f
+      static member SpritePosition'(self:p1) = !self.Position * 1.0f<pixel/m>
+      static member ScreenSize = Vector2<m>.One * 500.0f
 
-let start_game (game:StartGameArgs) =
+    and [<CasanovaEntity>] p2 = {
+         Position : Rule<Vector2<m>>
+         Velocity : Rule<Vector2<m/s>>
+         Sprite   : DrawableSprite
+    } with
+      static member Position'(self:p2,dt:float32<s>) = !self.Position + dt * !self.Velocity
+      static member Velocity'(self:p2,dt:float32<s>) = !self.Velocity * 0.9f
+      static member SpritePosition'(self:p2) = !self.Position * 1.0f<pixel/m>
+      static member ScreenSize = Vector2<m>.One * 500.0f
+
+  and [<CasanovaEntity>] ball = {
+    Position: Rule<Vector2<m>>
+    Life : Var<float32>
+    Sprite : DrawableSprite
+
+  } with
+    static member Position'(self:ball,dt:float32<s>) = !self.Position
+    static member SpritePosition'(self:ball) = !self.Position * 1.0f<pixel/m>
+    
+   //ADDED THE WALLS HERE
+   and [<CasanovaEntity>] wall = {
+        Position : Rule<Vector2<m>>
+        Sprite : DrawableSprite
+    } with
+    static member Position'(self:wall, dt:float32<s>) = !self.Position
+    static member SpritePosition'(self:wall) = !self.Position * 1.0f<pixel/m>
+
+   and [<CasanovaEntity>] wall2 = {
+        Position : Rule<Vector2<m>>
+        Sprite : DrawableSprite
+    } with
+    static member Position'(self:wall2, dt:float32<s>) = !self.Position
+    static member SpritePosition'(self:wall2) = !self.Position * 1.0f<pixel/m>
+
+ let start_game (game:StartGameArgs) =
 
   let world0 = 
     {
-      Bar = 
+      p1 = 
         {
-          Position = Rule.Create(Vector2<m>(490.0f<m>,0.0f<m>))
+          Position = Rule.Create(Vector2<m>(-450.0f<m>, 0.0f<m>))
           Velocity = Rule.Create(Vector2<m/s>.Zero)
-          Sprite   = DrawableSprite.Create(game.default_layer, Vector2<pixel>.One, Vector2<pixel>.One * 100.0f, @"p1")
+          Sprite   = DrawableSprite.Create(game.default_layer, Vector2<pixel>.One, Vector2<pixel>.One * 200.0f, @"p1")
         }
+
+      p2 = 
+        {
+          Position = Rule.Create(Vector2<m>(450.0f<m>, 0.0f<m>))
+          Velocity = Rule.Create(Vector2<m/s>.Zero)
+          Sprite   = DrawableSprite.Create(game.default_layer, Vector2<pixel>.One, Vector2<pixel>.One * 200.0f, @"p2")
+        }
+
+      wall = 
+        {
+          Position = Rule.Create(Vector2<m>(480.0f<m>, 0.0f<m>))
+          Sprite   = DrawableSprite.Create(game.default_layer, Vector2<pixel>.One, Vector2<pixel>.One * 1200.0f, @"blocker")
+        }
+
+      wall2 = 
+        {
+          Position = Rule.Create(Vector2<m>(-480.0f<m>, 0.0f<m>))
+          Sprite   = DrawableSprite.Create(game.default_layer, Vector2<pixel>.One, Vector2<pixel>.One * 1200.0f, @"blocker")
+        }
+
+      ball = 
+      {
+          Position = Rule.Create(Vector2<m>(0.0f<m>, 0.0f<m>))
+          Sprite   = DrawableSprite.Create(game.default_layer, Vector2<pixel>.One, Vector2<pixel>.One * 40.0f, @"ball")
+          Life = Var.Create(1.0f)
+      }
     }
   let inline (!) x = immediate_lookup x
   let main = yield_
-  
   let input = 
     [
       wait_key_press Keys.Escape  =>> game.quit()
       wait_key_press Keys.F9      =>> game.save "savefile"
       wait_key_press Keys.F10     =>> game.load "savefile"
-     // wait_key_down Keys.D        => co{ world0.Bar.Velocity := !world0.Bar.Velocity + Vector2<m/s>.UnitX * 100.0f }
-     // wait_key_down Keys.A        => co{ world0.Bar.Velocity := !world0.Bar.Velocity - Vector2<m/s>.UnitX * 100.0f }
-      wait_key_down Keys.W        => co{ world0.Bar.Velocity := !world0.Bar.Velocity - Vector2<m/s>.UnitY * 100.0f }
-      wait_key_down Keys.S        => co{ world0.Bar.Velocity := !world0.Bar.Velocity + Vector2<m/s>.UnitY * 100.0f }
+      //wait_key_down Keys.D        => co{ world0.p1.Velocity := !world0.p1.Velocity + Vector2<m/s>.UnitX * 100.0f }
+      //wait_key_down Keys.A        => co{ world0.p1.Velocity := !world0.p1.Velocity - Vector2<m/s>.UnitX * 100.0f }
+      wait_key_down Keys.W        => co{ world0.p1.Velocity := !world0.p1.Velocity - Vector2<m/s>.UnitY * 100.0f }
+      wait_key_down Keys.S        => co{ world0.p1.Velocity := !world0.p1.Velocity + Vector2<m/s>.UnitY * 100.0f }
+      wait_key_down Keys.Up        => co{ world0.p2.Velocity := !world0.p2.Velocity - Vector2<m/s>.UnitY * 100.0f }
+      wait_key_down Keys.Down        => co{ world0.p2.Velocity := !world0.p2.Velocity + Vector2<m/s>.UnitY * 100.0f }
     ]
   world0,main,input
 
